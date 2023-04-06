6119d5d82ef98567082f1cf57007a48285316361
mwchoen <sglee@cudo.co.kr>
2023-04-06 17:57:20 +0900

    싱글톤 패턴 구현 깨뜨리는 방법 1 - 리플렉션

--- src/main/java/me/mwcheon/designpatterns/DesignpatternsApplication.java
+++ src/main/java/me/mwcheon/designpatterns/DesignpatternsApplication.java
@@ -1,5 +1,6 @@
 package me.mwcheon.designpatterns;
 
+import me.mwcheon.designpatterns._01_creational_patterns.myTest.TestClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesignpatternsApplication {
 
 	public static void main(String[] args) {
+
+		TestClass t1 = TestClass.getInstance();
+		TestClass t2 = TestClass.getInstance();
+
+		System.out.println(t1 == t2);
+
 		SpringApplication.run(DesignpatternsApplication.class, args);
 	}
 
--- src/main/java/me/mwcheon/designpatterns/_01_creational_patterns/App.java
+++ src/main/java/me/mwcheon/designpatterns/_01_creational_patterns/App.java
@@ -1,7 +1,10 @@
 package me.mwcheon.designpatterns._01_creational_patterns;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
 //        new Setting(); // private로 생성자를 만들어 외부에서 new로 생성 하지 못 함
 
@@ -9,6 +12,18 @@ public class App {
         Settings settings2 = Settings.getInstance();
 
         System.out.println(settings1 == settings2);
+
+
+
+
+        // 1. 싱글톤 패턴 구현 방법을 깨뜨리는 방법 - 리플랙션 사용하기
+        Settings s1 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings newSettings = constructor.newInstance();
+
+        System.out.println(s1 != newSettings);
 
     }
 }
